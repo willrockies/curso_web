8922ddae65b49e14688503274fa0f8df3194bf54
W. Alves da Silva <w.alves.da.silva@avanade.com>
2020-01-11 20:15:42 -0300

    aulas sobre objeto finalizada

---
+++ fundamentos/objeto/oo.js
@@ -0,0 +1,18 @@
+// Procedural
+function processamento(valor1, valor2, valor3) {}
+
+//OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {},
+};
+
+objeto.processamento(); // Foco Passou a ser o objeto
+
+// Importante:
+// Abstração
+// Encapsulamento
+// Herança (Prototype)
+// Polimorfismo
