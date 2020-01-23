11205ac76a5ed839f2c12a8568167c6613d53f5d
W. Alves da Silva <w.alves.da.silva@avanade.com>
2020-01-22 23:50:23 -0300

    first commit on branch esnext

--- fundamentos/array/foreach2.js
+++ fundamentos/array/foreach2.js
@@ -8,3 +8,23 @@ const aprovados = ["Agatha", "Aldo", "Daniel", "Raquel"];
 aprovados.forEach2(function(nome, indice) {
   console.log(`${indice + 1}) ${nome}`);
 });
+
+const arrayPokemon = [
+  { nome: "Pikachu", forca: 10, ataque: 50 },
+  { nome: "Charizard", forca: 10, ataque: 60 },
+  { nome: "Squirtle", forca: 9, ataque: 40 },
+];
+// RPG de pokemon
+Array.prototype.forEachDinamico = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i);
+  }
+};
+
+arrayPokemon.forEachDinamico(function(nomePokemon, indice) {
+  console.log(
+    `${indice + 1}. ${nomePokemon.nome} ${nomePokemon.forca} ${
+      nomePokemon.ataque
+    }`,
+  );
+});
