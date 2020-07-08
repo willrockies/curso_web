f77ff2e21057816987dd8eaab0ee16d7640137ec
Wilson Alves <wlsn.ads@gmail.com>
2020-07-07 22:17:10 -0300

    fetch/axios

--- ajax/server.js
+++ ajax/server.js
@@ -29,4 +29,20 @@ app.post('/upload', (req, res) => {
 })
 
 app.get('/teste', (req, res) => res.send('OK'));
+
+app.post('/formulario', (req, res) => {
+  res.send({
+    ...req.body,
+    id: 1
+  });
+});
+
+app.get('/parOuImpar/', (req, res) => {
+  
+  const par = parseInt(req.query.numero) % 2 === 0;
+
+  res.send({
+    resultado: par ? 'par' : 'impar'
+  });
+})
 app.listen(8080, () => console.log('Executando'));
