7d8d198a1f380cade44d1c3392635c7996d793fe
willrockies <wlsn.ads@gmail.com>
2022-05-09 12:26:38 -0300

    fundamentos e removido arquivos e cap não utilizado

--- fundamentos/array/145 - concat.js
+++
@@ -1,5 +0,0 @@
-const filhas = ["Ualeskah", "Cibalena"];
-const filhos = ["Uoxiton", "Uesclei"];
-const todos = filhas.concat(filhos, "Fulano");
-console.log(todos, filhas, filhos);
-console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
