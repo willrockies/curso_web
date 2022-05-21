7ad5c46a3960785fa8c6319e429ea038602ea707
willrockies <wlsn.ads@gmail.com>
2022-05-21 00:04:22 -0300

    first commit

--- funcao/109 - desafioFuncaoConstrutora.js
+++
@@ -1,9 +0,0 @@
-function Pessoa(nome){
-  this.nome = nome
-  this.falar = function(){
-    return `Meu nome é ${this.nome}`
-  }
-}
-
-const criarPessoa = new Pessoa('João');
-console.log(criarPessoa.falar())
--- funcao/92 - thisPodeVariar.js
+++
@@ -1,5 +0,0 @@
-// testar no navegador(browser0)
-function f1(){console.log(this === window)}
-console.log(f1())
-function f2(){console.log(this === global)}
-
