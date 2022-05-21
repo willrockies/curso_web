2ed918a8ad16fb4eed27b9e71489a481a6f1790a
willrockies <wlsn.ads@gmail.com>
2022-05-21 00:19:51 -0300

    sistema de modulos

---
+++ nodejs/150 - moduloCliente.js
@@ -0,0 +1,9 @@
+const moduleA = require('./150 - moduloA')
+const moduleB = require('./150 - moduloB')
+
+console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.ateLogo)
+
+console.log(moduleB.bomDia)
+console.log(moduleB.boaNoite())
